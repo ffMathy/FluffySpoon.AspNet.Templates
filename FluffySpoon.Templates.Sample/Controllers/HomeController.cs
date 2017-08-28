0771986502ceddac2ce9042f9bae3a3ffb991b33
Mathias Lorenzen <mlo@dandomain.dk>
2017-08-28 14:50:35 +0200

    added sample site.

---
+++ FluffySpoon.Templates.Sample/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FluffySpoon.Templates.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
